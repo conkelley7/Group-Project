af0967f1cd34e5e206dd13348326f37134e497ec
chris <chris@10.0.0.10>
2024-11-29 13:26:28 -0600

    First Commit

---
+++ Simulation/src/main/java/com/mycompany/simulation/Simulation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.simulation;
+
+/**
+ *
+ * @author chris
+ */
+public class Simulation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
