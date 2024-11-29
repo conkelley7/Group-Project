84a0e9953070e0c621c6da618226b12b26f98c72
chris <chris@10.0.0.10>
2024-11-29 14:10:50 -0600

    First Commit

--- Simulation/src/main/java/Character.java
+++ Simulation/src/main/java/Character.java
@@ -9,4 +9,42 @@
  */
 public class Character {
     
+    private int health;
+    private int attack;
+    public boolean alive;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+    
+    
+    public int attacked(int damage){
+        
+        return damage;   //probably will be changed later 
+    }
+    public int attacking(){
+        
+        return attack;  //probably will be changed later 
+    }
+    
 }
